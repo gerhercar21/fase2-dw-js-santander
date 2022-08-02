e992b7a524c015ac36c03d35af36f7aedbf3a319
Gerardo <gerardohdezcarrera@gmail.com>
2022-08-01 23:30:08 -0500

    Ejercicios Curso Youtube

---
+++ exercises.js
@@ -0,0 +1,73 @@
+//var mostrarMensaje = true;
+
+//if(mostrarMensaje) {
+//  document.write("hola mundo!");
+//} else {
+//  document.write("mostrarMensaje es Falso");
+//}
+
+//var edad = 25;
+
+//if(edad < 13) {
+//  document.write("Eres un niño");
+//} else if(edad < 19){
+//  document.write("Eres un adolescente");
+//} else if(edad < 35) {
+//    document.write("¡Ánimo! Todavía eres joven");
+//} else {
+//    document.write("Empieza a cuidarte, ya no eres un niño");
+// }
+
+//var mensaje = "Soy un mensaje";
+
+//for(var i = 0; i < 5; i++) {
+//  document.write(i + " " + mensaje + " en JavaScript <br>");
+//};
+
+//var dias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", //"domingo"];
+
+//for(var i = 0; i < 7; i ++){
+//  document.write(dias[i] + "<br>");
+//}
+
+//var dias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo", "y otro dia mas", "muchos mas", "365 días"];
+
+//for(i in dias) {
+//  document.write(dias[i] + "<br>")
+//}
+
+//var cadena = "Hola mi mundo mañanero";
+//var cadena2 = " soy una cadena";
+
+//var resultado = cadena.indexOf("w");
+
+//if(resultado >= 0) {
+//  document.write("Muy bien. El caracter es valido")
+//} else {
+//  document.write("Lo sentimos, caracter inválido. Inténtalo nuevamente")
+//}
+
+
+//document.write(resultado);
+
+//var dias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
+//var arr = ["enero", "febrero", 2, true]
+
+//var resultado = dias.length; cuenta elementos
+// var resultado = dias.concat(arr); Une
+//var resultado = dias.join(" "); Crea cadenas uniendo los elementos
+// var resultado = dias.pop(); //extra eel valor y lo elimina del original
+//var resultado = dias.push(); // Elimina y agrega elemento en posición, agrega al final al array
+//var resultado = dias.unshift("enero"); // agrega al inicio del array
+//var resultado = dias.reverse(); pone al revés
+
+//document.write(dias + " -> Array Original <hr>");
+//document.write(resultado + " -> Resultado");
+
+var numero = 2345.987602;
+
+var resultadoNum = NaN;
+document.write(numero + " -> Número original <br>");
+document.write(resultadoNum + " -> Resultado");
+
+// video 15 https://www.youtube.com/watch?v=yzljtuK4QJ0
